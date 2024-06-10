4a33f46539f236f006a7aa8225480637d5062876
ritamganguli <35348707+ritamganguli@users.noreply.github.com>
2024-06-10 21:24:24 +0530

    Update Test4.java

--- src/test/java/Test4.java
+++ src/test/java/Test4.java
@@ -30,7 +30,7 @@ public class Test4
         
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("build", "HyperExecute -Demonstration of the TestNG Framework");
-        capabilities.setCapability("name", "[HyperExecute - 4] Demonstration of the TestNG Framework");
+        capabilities.setCapability("build", System.getenv("LT_BUILD_NAME"));
 
         capabilities.setCapability("platform", System.getenv("HYPEREXECUTE_PLATFORM"));
         capabilities.setCapability("browserName", browser);
